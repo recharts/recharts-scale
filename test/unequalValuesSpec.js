d8b19c393a34ce9a220cf74dcbabd44d6e606080
xile611 <327197681@qq.com>
2015-08-25 14:28:46 +0800

    test:add test case

--- test/unequalValuesSpec.js
+++ test/unequalValuesSpec.js
@@ -56,4 +56,22 @@ describe('unequal values', () => {
     });
 
   });
+
+  describe('wrong min & max', () => {
+    let [min, max, count] = [67, 5, 5],
+        scales = scale(min, max, count);
+
+    it('should return scales of length count', () => {
+      expect(scales).to.have.length(count);
+    });
+
+    it('should scales[0] is not bigger than min', () => {
+      expect(max).to.not.below(scales[0]);
+    });
+
+    it('should last elemtnt of scales is not smaller than min', () => {
+      expect(min).to.not.above(scales[scales.length - 1]);
+    });
+
+  });
 });
