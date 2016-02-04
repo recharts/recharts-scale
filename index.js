7c5cd66576a564c85f8c686382b9efb64ab1c95e
xile611 <dingling112@gmail.com>
2016-02-04 17:28:41 +0800

    chore: optimize npm script commands

--- index.js
+++
@@ -1,7 +0,0 @@
-import getTickValues from './src/getTickValues';
-import getNiceTickValues from './src/getNiceTickValues';
-
-export {
-  getTickValues,
-  getNiceTickValues,
-};
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+
+module.exports = {
+  output: {
+    library: 'RechartsScale',
+    libraryTarget: 'umd',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
+  node: {
+    Buffer: false
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
